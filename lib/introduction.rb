2e2c5f8fba21b4496dc6d522c5f055bfc3a4b17f
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-23 00:17:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,13 +2,15 @@ def "#introduction"(name)
 puts `"Hi, my name is #{name}."
 end
 
-(
-def '#introduction_with_language`) that takes in two arguments,
-`name` and `language` and outputs the phrase: "Hi, my name is #{name} and I am
+
+def '#introduction_with_language` (name, language)
+puts "Hi, my name is #{name} and I am
 learning to program in #{language}."
+end
 
-## Define a Method With an Optional Argument
 
-Copy the previous method and name it `#introduction_with_language_optional`. It
-that takes in two arguments,`name` and `language`, but language is optional. The
-default value for `language` will be "Ruby".
+
+def `#introduction_with_language_optional`(name, language = "Ruby") 
+puts "Hi, my name is #{name} and I am
+learning to program in #{language}."
+end
